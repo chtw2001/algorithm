e5160ceca68f2b5c90d8c8a0cde7b3fa1a4aaf18
chtw2001 <jungtaekwon1019@gmail.com>
2023-09-16 12:52:38 +0900

    23/09/16

--- september_2nd/25512(un).py
+++ september_2nd/25512(un).py
@@ -7,15 +7,19 @@ depth = [0]*n
 cost = [0]*n
 
 
-def dfs(_list, length, idx, depth):
+def dfs(_list, length, idx, d):
     if idx == length:
-        depth[_list[idx]] = depth % 2
-        return 
+        depth[idx - 1] = d % 2
+        return
+    
+    if tree[_list[0]]:
+        dfs(tree[_list[0]], len(tree[_list[0]]), 0, d+1)
+    
+    dfs(tree[_list[idx+1]], len(tree[_list[idx+1]]), idx+1, d)
 
-
+dfs(tree[0], len(tree[0]), 0, 0)
 for _ in range(n-1):
     m, s = map(int, input().split())
-    # depth[s] = depth[m] + 1
     tree[m].append(s)
 
 for i in range(n):
@@ -24,7 +28,7 @@ for i in range(n):
 
 odd_w, odd_b, even_w, even_b = 0, 0, 0, 0
 for i in range(n):
-    if depth[i] % 2:
+    if depth[i]:
         even_w += cost[i][0]
         even_b += cost[i][1]
     else:
