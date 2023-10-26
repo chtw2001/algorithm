a4150c09b2ba010c33b57b19808e6fd44779f459
chtw2001 <jungtaekwon1019@gmail.com>
2023-10-27 05:51:15 +0900

    23/10/27

--- ex.py
+++ ex.py
@@ -1,18 +1,46 @@
 import sys, math
 from collections import defaultdict, deque
 from heapq import heappush as push, heappop as pop
-import turtle
+import turtle, datetime
 
+for i in range(6):
+    for j in range(6):
+        print("*", end='')
+    print()
+print()
 
-turtle.right(90)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+for i in range(6):
+    for j in range(i+1):
+        print("*", end='')
+    print()
+print()
 
+for i in range(6):
+    for j in range(6-i):
+        print("*", end='')
+    print()
+print()
+
+for i in range(6):
+    for j in range(i):
+        print(' ', end='')
+    for j in range(6-i):
+        print('*', end='')
+    print()
+print()
+
+for i in range(6):
+    for j in range(6-i):
+        print(' ', end='')
+    for j in range(i+1):
+        print('*', end='')
+    print()
+print()
+
+for i in range(6):
+    for j in range(6-i-1):
+        print(' ', end='')
+    for j in range(i*2+1):
+        print('*', end='')
+    print()
+print()
