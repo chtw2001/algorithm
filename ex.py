e204398376719b0768c8100ab3f23b86f9e99cdd
chtw2001 <jungtaekwon1019@gmail.com>
2023-12-26 20:56:00 +0900

    2023/12/26.

--- ex.py
+++ ex.py
@@ -3,6 +3,20 @@ import math
 from collections import defaultdict, deque
 from heapq import heappush as push, heappop as pop
 
+a = [1,2]
+a[-1:] = [40]
+print(a)
+
+a = [1,2]
+a[len(a):] = [40]
+print(a)
+print(1)
+
+a = [1,2]
+a[0:] = [40]
+a.pop()
+print(a)
+
 a = 1
 for i in range(1, 11):
     a *=i
