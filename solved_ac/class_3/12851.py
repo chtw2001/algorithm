77b68ee3ceb97bd57078d60b6c7f9ff622b1ec97
chtw2001 <jungtaekwon1019@gmail.com>
2024-01-09 18:00:04 +0900

    2023/01/09

--- solved_ac/class_3/12851.py
+++ solved_ac/class_3/12851.py
@@ -1,4 +1,4 @@
-from collections import deque, defaultdict
+from collections import deque
 N, K = map(int, input().split())
 
 if N >= K:
@@ -6,39 +6,24 @@ if N >= K:
     print(1)
     quit()
 
+visited = [0]*100001
 q = deque()
-q.append((N, 0))
-visited = set()
-answ_dict = defaultdict(int)
+q.append(N)
+cnt = 0
 
 while q:
-    ex, num = q.popleft()
-    if ex == 3 and num ==2 :
-        print(1)
-    if ex in visited and not answ_dict[num]:
-        continue
-    
-    if ex < 0 or ex > 100000:
-        continue
-    
-    visited.add(ex)
+    ex = q.popleft()
+
     if ex == K:
-        answ_dict[num] += 1
+        cnt += 1
         continue
-    
-    if ex > K:
-        q.append((ex-1, num+1))
-        continue
-    
-    q.append((ex+1, num+1))
-    q.append((ex-1, num+1))
-    q.append((ex*2, num+1))
 
+    ary = [ex-1, ex+1, ex*2]
+    for i in ary:
+        if 0 <= i < 100001:
+            if not visited[i] or visited[i] >= visited[ex]+1:
+                visited[i] = visited[ex] + 1
+                q.append(i)
 
-answ = 0
-for key in sorted(answ_dict.keys()):
-    if answ_dict[key]:
-        answ = key
-
-print(answ)
-print(answ_dict[answ])
+print(visited[K])
+print(cnt)
